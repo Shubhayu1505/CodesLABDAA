c61f6068d645775146a86e14f35e4fc0dc0fa47f
Shubhayu K <161205746+Shubhayu1505@users.noreply.github.com>
2025-01-30 04:00:14 +0530

    main

--- ex_2_program_a.c
+++ ex_2_program_a.c
@@ -1,27 +1,5 @@
 #include <stdio.h>
 
-void insertElement(int arr[], int *size, int element) {
-    arr[*size] = element;
-    (*size)++;
-}
-
-void removeElement(int arr[], int *size, int element) {
-    int i, found = 0;
-    for (i = 0; i < *size; i++) {
-        if (arr[i] == element) {
-            found = 1;
-            for (int j = i; j < *size - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-            (*size)--;
-            break;
-        }
-    }
-    if (!found) {
-        printf("Element not found!\n");
-    }
-}
-
 int linearSearch(int arr[], int size, int element) {
     for (int i = 0; i < size; i++) {
         if (arr[i] == element) {
@@ -40,30 +18,29 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[100], size = 0, choice, element, index;
+    int arr[100], size, element, index, choice;
 
+    // Input array size and elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &size);
+    printf("Enter the elements:\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Display the array
+    printArray(arr, size);
+
+    // Menu for linear search and option to repeat
     while (1) {
-        printf("\nMenu:\n");
-        printf("1. Insert Element\n");
-        printf("2. Remove Element\n");
-        printf("3. Search using Linear Search\n");
-        printf("4. Display Array\n");
-        printf("5. Exit\n");
+        printf("\nSearch Menu:\n");
+        printf("1. Search using Linear Search\n");
+        printf("2. Quit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("Enter element to insert: ");
-                scanf("%d", &element);
-                insertElement(arr, &size, element);
-                break;
-            case 2:
-                printf("Enter element to remove: ");
-                scanf("%d", &element);
-                removeElement(arr, &size, element);
-                break;
-            case 3:
                 printf("Enter element to search for: ");
                 scanf("%d", &element);
                 index = linearSearch(arr, size, element);
@@ -73,10 +50,7 @@ int main() {
                     printf("Element not found.\n");
                 }
                 break;
-            case 4:
-                printArray(arr, size);
-                break;
-            case 5:
+            case 2:
                 return 0;
             default:
                 printf("Invalid choice. Please try again.\n");
