79110036ab2abb9d1ce6c500e515dd5d303d604d
Shubhayu K <161205746+Shubhayu1505@users.noreply.github.com>
2025-01-30 04:01:00 +0530

    main

--- ex_3_program.c
+++ ex_3_program.c
@@ -1,85 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void insertElement(int arr[], int *size, int element) {
-    arr[*size] = element;
-    (*size)++;
-}
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
 
-void removeElement(int arr[], int *size, int element) {
-    int i, found = 0;
-    for (i = 0; i < *size; i++) {
-        if (arr[i] == element) {
-            found = 1;
-            for (int j = i; j < *size - 1; j++) {
-                arr[j] = arr[j + 1];
-            }
-            (*size)--;
-            break;
-        }
-    }
-    if (!found) {
-        printf("Element not found!\n");
-    }
-}
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
-void merge(int arr[], int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-
-    // Dynamically allocate memory for L and R
-    int *L = (int*)malloc(n1 * sizeof(int));
-    int *R = (int*)malloc(n2 * sizeof(int));
-
-    for (int i = 0; i < n1; i++) {
-        L[i] = arr[l + i];
-    }
-    for (int j = 0; j < n2; j++) {
-        R[j] = arr[m + 1 + j];
-    }
-
-    int i = 0, j = 0, k = l;
+    int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
     }
 
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-
-    // Free dynamically allocated memory
-    free(L);
-    free(R);
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        merge(arr, l, m, r);
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
 void printArray(int arr[], int size) {
-    printf("Array: ");
     for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
@@ -87,42 +35,20 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[100], size = 0, choice, element;
+    int arr[100], size;
 
-    while (1) {
-        printf("\nMenu:\n");
-        printf("1. Insert Element\n");
-        printf("2. Remove Element\n");
-        printf("3. Sort using Merge Sort\n");
-        printf("4. Display Array\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+    printf("Enter number of elements: ");
+    scanf("%d", &size);
 
-        switch (choice) {
-            case 1:
-                printf("Enter element to insert: ");
-                scanf("%d", &element);
-                insertElement(arr, &size, element);
-                break;
-            case 2:
-                printf("Enter element to remove: ");
-                scanf("%d", &element);
-                removeElement(arr, &size, element);
-                break;
-            case 3:
-                mergeSort(arr, 0, size - 1);
-                printf("Array sorted using Merge Sort.\n");
-                break;
-            case 4:
-                printArray(arr, size);
-                break;
-            case 5:
-                return 0;
-            default:
-                printf("Invalid choice. Please try again.\n");
-        }
+    printf("Enter elements: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
     }
+
+    mergeSort(arr, 0, size - 1);
+
+    printf("Sorted array using Merge Sort: ");
+    printArray(arr, size);
 
     return 0;
 }
